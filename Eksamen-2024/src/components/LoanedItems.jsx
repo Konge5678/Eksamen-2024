134a0ad4a3d82d7187d5c87171cdcc91275b6cec
KrisH <123541182+Konge5678@users.noreply.github.com>
2024-06-04 20:12:33 +0200

    Lagt til innkjøpsdato og innkjøpspris på produktene

--- Eksamen-2024/src/components/LoanedItems.jsx
+++ Eksamen-2024/src/components/LoanedItems.jsx
@@ -3,41 +3,39 @@ import { useEquipment } from '../context/EquipmentContext';
 import { useAuth } from '../context/AuthContext';
 
 const LoanedItems = () => {
-  const { handleReturn } = useEquipment();
+  const { equipment, handleReturn } = useEquipment();
   const { isAdmin } = useAuth();
-  const lentItems = JSON.parse(localStorage.getItem('lentItems')) || [];
   const [searchTerm, setSearchTerm] = useState('');
 
-  const filteredItems = lentItems.filter(item =>
+  const lentItems = JSON.parse(localStorage.getItem('lentItems')) || [];
+
+  const filteredLentItems = lentItems.filter(item => 
     item.userName.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
-  const handleReturnItem = (item) => {
-    handleReturn(item);
-    setSearchTerm(searchTerm => searchTerm + ' ');
-  };
 
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-xl font-bold mb-4">Lånte produkter</h2>
-      <input
-        type="text"
-        placeholder="Søk etter navn"
+      <input 
+        type="text" 
+        placeholder="Søk etter bruker" 
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="border p-2 mb-4"
+        className="border p-2 mb-4 w-full"
       />
-      {filteredItems.length > 0 ? (
-        filteredItems.map((item, index) => (
+      {filteredLentItems.length > 0 ? (
+        filteredLentItems.map((item, index) => (
           <div key={index} className="border bg-gray-300 rounded-md p-4 mb-2">
             <h2 className="text-xl font-bold">{item.Beskrivelse}</h2>
             <p><strong>Produsent:</strong> {item.Produsent}</p>
             <p><strong>Spesifikasjoner:</strong> {item.Spesifikasjoner}</p>
             <p><strong>Kategori:</strong> {item.Kategori}</p>
+            <p><strong>Innkjøpspris:</strong> {item['Innkjøpspris']} Kr</p>
+            <p><strong>Innkjøpsdato:</strong> {item['Innkjøpsdato']}</p>
             <p><strong>Lånt av:</strong> {item.userName}</p>
             {isAdmin && <p><strong>Telefon:</strong> {item.userPhone}</p>}
             <button
-              onClick={() => handleReturnItem(item)}
+              onClick={() => handleReturn(item)}
               className="bg-red-500 text-white rounded px-4 py-2 mt-2"
             >
               Returner
@@ -45,7 +43,7 @@ const LoanedItems = () => {
           </div>
         ))
       ) : (
-        <p className='mb-80'>Ingen produkter lånt ut.</p>
+        <p>Ingen produkter lånt ut.</p>
       )}
     </div>
   );
